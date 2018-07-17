660438a01f8c3af6652357a08aa456bbd8d41c74
bian2510 <bian251091@gmail.com>
2018-07-17 18:11:11 -0300

    RETO COMPLETO

--- morse.rb
+++ morse.rb
@@ -1,97 +1,87 @@
 class Morse
-		
-	def initialize()
-		@alfabeto = { 
-			"a" => ".-",
-			"b" => "-...",
-			"c" => "-.-.",
-			"d" => "-..",
-			"e" => ".",
-			"f" => "..-.",
-			"g" => "--.",
-			"h" => "....",
-			"i" => "..",
-			"j" => ".---",
-			"k" => "-.-",
-			"l" => ".-..",
-			"m" => "--",
-			"n" => "-.",
-			"o" => "---",
-			"p" => ".--.",
-			"q" => "--.-",
-			"r" => ".-.",
-			"s" => "...",
-			"t" => "-",
-			"u" => "..-",
-			"v" => "...-",
-			"w" => ".--",
-			"x" => "-..-",
-			"y" => "-.--",
-			"z" => "--.."}
+	ALFABETO = { 
+		"a" => ".-",
+		"b" => "-...",
+		"c" => "-.-.",
+		"d" => "-..",
+		"e" => ".",
+		"f" => "..-.",
+		"g" => "--.",
+		"h" => "....",
+		"i" => "..",
+		"j" => ".---",
+		"k" => "-.-",
+		"l" => ".-..",
+		"m" => "--",
+		"n" => "-.",
+		"o" => "---",
+		"p" => ".--.",
+		"q" => "--.-",
+		"r" => ".-.",
+		"s" => "...",
+		"t" => "-",
+		"u" => "..-",
+		"v" => "...-",
+		"w" => ".--",
+		"x" => "-..-",
+		"y" => "-.--",
+		"z" => "--.."
+	}.freeze
 
-		@error = 
-			{"error1" => "..--",
+	def initialize()
+		@alfabeto = ALFABETO
+		@error = {
+			"error1" => "..--",
 			"error2" => ".-.-",
 			"error3" => "---.",
-			"error4" => "----"}
-
-		def start
-			a = ""
-			b = ""
-			largo_combinaciones =[]
-			posibles_combinaciones = []
-			input = gets.chomp.to_s
-			@length_letras = input.length
-
-			while @length_letras > 10 || @length_letras == 0
-				input = gets.chomp.to_s
-				@length_letras = input.length
-			end
-
-			letras = input.split("")
-			count = 0
-			@morse = ""
-
-			while count < @length_letras
-				@morse << @alfabeto[letras[count]]
-				count = count + 1
-			end
-
-			@length_morse = @morse.length
-			puts @morse
-
-
-
-
-				
-			@count2 = 0
-			@count3 = 0
-			combinaciones
-		end
-
-		def combinaciones
-			@alfabeto.each {|key, value|
-				puts key
-				if value == @morse[0..value.length-1]
-					puts value
-					puts key
-					@morse = @morse[value.length..-1]
-					puts @morse
-					puts @count2 += 1
-					if @morse == "" && @count2 == @length_letras
-						count3 += 1
-					end
-					combinaciones
-					puts "$"
-				end
-			}
-			puts count3
-		end
-
+			"error4" => "----"
+		}
 	end
 	
+	def start
+		input = gets.chomp.to_s
+		@length_letras = input.length
+
+		letras = input.split("")
+		count = 0
+		@inicio = 0	
+		@count2 = 0
+		@count3 = 0
+		@morse = ""
+
+		while count < @length_letras
+			@morse << @alfabeto[letras[count]]
+			count = count + 1
+		end
+
+		@length_morse = @morse.length
+		combinaciones
+		puts @count3
+	end
+
+	def combinaciones
+		@alfabeto.each do |key, value|
+			if @morse[@inicio..-1] != ""
+				if value == @morse[@inicio..@inicio+value.length-1]
+					@count2 += 1
+					@coincidencia = value
+					@inicio = @inicio + value.length
+					if @morse[@inicio..-1] == "" && @count2 == @length_letras
+						@count3 += 1
+					end
+					combinaciones
+					@coincidencia = value
+					if @inicio != 0
+						@inicio = @inicio - @coincidencia.length
+					else
+						@inicio = 0
+					end
+				end
+			end
+		end
+		@count2 -= 1
+	end
 end
 
 object =  Morse.new
 object.start
-
